413b4920cee8002415b4e1392f7a187e8b2cd8dd
that devguy <zkmutch@gmail.com>
2023-07-16 16:35:48 -0400

    stubbed out navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav>
       <div>Navbar</div>
+      <div className="navbar--linksContainer flex gap-3">
+        <Link href="/">Home</Link>
+        <Link href="/reviews">Reviews</Link>
+        <Link href="/top-games">Top Games</Link>
+        <Link href="/new-releases">New Releases</Link>
+        <Link href="/browse">Browse</Link>
+      </div>
     </nav>
   );
 }
--- src/app/top-games/page.js
+++ src/app/top-games/page.js
@@ -1,8 +1,7 @@
 export default function TopGames() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>Top Games</div>
-      </main>
-    )
-  }
-  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Top Games</div>
+    </main>
+  );
+}
